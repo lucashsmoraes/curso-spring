7809697a004e2d4782570149106e1a27b3de9576
Lucas Moraes <lucashsmoraes@gmail.com>
2018-09-16 18:12:49 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/curso/spring/domain/Produto.java
+++ src/main/java/com/curso/spring/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -65,7 +67,7 @@ public class Produto implements Serializable{
 		return categorias;
 	}
 
-
+	@JsonBackReference
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
--- src/main/java/com/curso/spring/services/CategoriaService.java
+++ src/main/java/com/curso/spring/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.curso.spring.domain.Categoria;
 import com.curso.spring.repositories.CategoriaRepository;
+import com.curso.spring.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService{
@@ -14,6 +15,10 @@ public class CategoriaService{
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
