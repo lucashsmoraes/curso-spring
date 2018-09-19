5123a17d5fe291f9b7b450d8b002cd7ed496c861
Lucas Moraes <lucas.moraes@softplan.com.br>
2018-09-19 11:37:27 -0300

    PUT, DELETE e GET para Cliente

---
+++ src/main/java/com/curso/spring/dto/ClienteDTO.java
@@ -0,0 +1,55 @@
+package com.curso.spring.dto;
+
+import com.curso.spring.domain.Cliente;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import java.io.Serializable;
+
+public class ClienteDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
+    private String nome;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Email(message = "Preenchimento obrigatório")
+    private String email;
+
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(Cliente obj) {
+        this.setId(obj.getId());
+        this.setNome(obj.getNome());
+        this.setEmail(obj.getEmail());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/com/curso/spring/services/ClienteService.java
+++ src/main/java/com/curso/spring/services/ClienteService.java
@@ -1,10 +1,20 @@
 package com.curso.spring.services;
 
 import com.curso.spring.domain.Cliente;
+import com.curso.spring.dto.ClienteDTO;
 import com.curso.spring.repositories.ClienteRepository;
+import com.curso.spring.services.exceptions.DataIntegrityException;
 import com.curso.spring.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static sun.net.ftp.FtpReplyCode.find;
 
 @Service
 public class ClienteService {
@@ -12,7 +22,7 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Cliente obj = repo.findOne(id);
 		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
@@ -21,4 +31,38 @@ public class ClienteService {
 		return obj;
 	}
 
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Cliente update(Cliente obj){
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id){
+		find(id);
+		try {
+			repo.delete(id);
+		}
+		catch (DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir Cliente porque há entidades relacionadas");
+		}
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+
+	public Cliente fromDTO(ClienteDTO objDto){
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj){
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
