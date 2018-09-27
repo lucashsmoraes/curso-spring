99212f49056c7f0b7f8978646ceb404d12d0f9fd
Lucas Moraes <lucas.moraes@softplan.com.br>
2018-09-27 10:24:17 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/curso/spring/dto/ProdutoDTO.java
@@ -0,0 +1,45 @@
+package com.curso.spring.dto;
+
+import com.curso.spring.domain.Produto;
+
+import java.io.Serializable;
+
+public class ProdutoDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+    private double preco;
+
+    public ProdutoDTO(){}
+
+    public ProdutoDTO(Produto obj) {
+        id = obj.getId();
+        nome = obj.getNome();
+        preco = obj.getPreco();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
---
+++ src/main/java/com/curso/spring/services/ProdutoService.java
@@ -0,0 +1,39 @@
+package com.curso.spring.services;
+
+import com.curso.spring.domain.Categoria;
+import com.curso.spring.domain.Produto;
+import com.curso.spring.repositories.CategoriaRepository;
+import com.curso.spring.repositories.ProdutoRepository;
+import com.curso.spring.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Produto find(Integer id) {
+		Produto obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Produto.class.getName());
+		}
+		return obj;
+	}
+
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAll(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+
+}
