027e4bfa92c09b61dcd9b298f259d8e42fb6badb
Lucas Moraes <lucas.moraes@softplan.com.br>
2018-09-27 15:46:27 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/curso/spring/CursospringApplication.java
+++ src/main/java/com/curso/spring/CursospringApplication.java
@@ -1,13 +1,5 @@
 package com.curso.spring;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import com.curso.spring.domain.*;
-import com.curso.spring.enums.EstadoPagamento;
-import com.curso.spring.enums.TipoCliente;
-import com.curso.spring.repositories.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
