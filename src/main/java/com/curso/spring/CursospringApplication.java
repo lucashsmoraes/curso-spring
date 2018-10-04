eeb7a2031c56d87880ded4cf708a7b777be60222
Lucas Moraes <lucas.moraes@softplan.com.br>
2018-10-04 14:25:01 -0300

    Salvando primeiro arquivo no s3

--- src/main/java/com/curso/spring/CursospringApplication.java
+++ src/main/java/com/curso/spring/CursospringApplication.java
@@ -1,5 +1,7 @@
 package com.curso.spring;
 
+import com.curso.spring.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursospringApplication implements CommandLineRunner{
 	
-
+	@Autowired
+	private S3Service s3Service;
 
 
 	public static void main(String[] args) {
@@ -17,6 +20,8 @@ public class CursospringApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		s3Service.uploadFile("/home/lucasmoraes/Pictures/img1.jpg");
 
 	}
 }
