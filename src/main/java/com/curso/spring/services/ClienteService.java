1a9e1654c712853fc35214a61e9785e648488b57
Lucas Moraes <lucas.moraes@softplan.com.br>
2018-10-08 13:56:52 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/curso/spring/services/ClienteService.java
+++ src/main/java/com/curso/spring/services/ClienteService.java
@@ -75,6 +75,20 @@ public class ClienteService {
 		return repo.save(newObj);
 	}
 
+	public Cliente findByEmail(String email){
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(PerfilCliente.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+
+		Cliente obj = repo.findByEmail(email);
+		if (obj == null){
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + user.getId()
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
